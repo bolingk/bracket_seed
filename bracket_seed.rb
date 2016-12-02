2bcd3a9effa66079f9cef5b6b42473c538fbee0e
Kyle Boling <kmboling@gmail.com>
2016-12-02 14:31:42 -0500

    add matchup method

--- bracket_seed.rb
+++ bracket_seed.rb
@@ -5,6 +5,7 @@
 class Team
   attr_accessor :seed
   attr_accessor :school	
+
   def initialize(seed, school)
     @seed = seed
     @school = school	 	
@@ -16,6 +17,7 @@ def display_menu clear_menu
   if clear_menu == "yes"
   	system "clear"
   end
+
   puts "**********************************************************"
   puts "**********************************************************"
   puts "****Welcome to my tournament generator. Enter a selection:"
@@ -55,12 +57,13 @@ def ret_menu
     elsif ret == "n"
   	  exit_application
     else
-  	  display_menu("yes")
+      ret_menu
   	end  
 end
 
 
 def add_team
+  system "clear"
   puts "Enter school name (eg. UGA):"
   school = gets.chomp
   puts "Enter ranking:"
@@ -77,6 +80,7 @@ end
 
 
 def list_teams
+  system "clear"
   puts "The #{@team_seeds.length} teams are seeded as follows:"
   @team_seeds.each do |team|
     puts "#{team.seed}. #{team.school}"
@@ -86,16 +90,30 @@ end
 
 
 def matchups
+  system "clear"
+  bottom_seed = @team_seeds.length
+
   if @team_seeds.length % 2 == 0
-    puts "Even number of teams"
-    # unshift element + pop element
-    # somehow access @seed and @school attributes
-    # delete those two elements from local array
+    top_seed = 1
+    middle = @team_seeds.length/2
+    second_tier = middle + 1
   else
-    puts "Odd number of teams"
-    # unshift one seed from array
-    # then carry out the if expression on remaining.
-    # delete from local array
+    bye = 1
+    top_seed = 2
+    middle = (@team_seeds.length-1)/2
+    second_tier = middle + 1
+  end
+
+  puts "Matchups are as follows:"
+
+  while top_seed < second_tier do
+    if bye == 1
+      puts "(#{bye})#{@team_seeds[bye]} bye week first round"
+    end
+
+    puts "(#{top_seed})#{@team_seeds[1]} against (#{bottom_seed})#{@team_seeds[2]}"
+    top_seed += 1
+    bottom_seed -= 1
   end
   ret_menu
 end
